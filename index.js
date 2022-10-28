b52b60b324ec9005077635ed45055ab7de1e6600
Auditi <auditibarua6@gmail.com>
2022-10-28 19:05:10 +0300

    added idex.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-const categories = require('./categories.json');
-
+const categories = require('./Data/categories.json');
+const information = require('./Data/information.json');
 
 app.get('/', (req, res) => {
     res.send('api running')
@@ -14,6 +14,17 @@ app.get('/', (req, res) => {
 app.get('/categories', (req, res) => {
     res.send(categories);
 })
+
+app.get('/information', (req, res) => {
+    res.send(information);
+})
+app.get('/bycategory/:id', (req, res) => {
+    const id = req.params.id;
+    const courseItem = information.filter(item => item.id == id);
+    res.send(courseItem);
+});
+
+
 
 app.listen(port, () => {
     console.log('programmin server running', port)
